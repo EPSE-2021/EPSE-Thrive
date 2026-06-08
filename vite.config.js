72df53b05e74a8f5a983fc981414121ee80805f6
EPSE-2021 <Feliciasoodeen@EPSEEducationProtectionS028.onmicrosoft.com>
2026-06-08 16:44:53 -0400

    Update GitHub Pages URL and repository name

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Your GitHub Pages URL: https://epse-2021.github.io/econnectyouth/
+// Your GitHub Pages URL: https://epse-2021.github.io/EPSE-Thrive/
 // REPO_NAME must exactly match your repository name on GitHub.
-const REPO_NAME = "/econnectyouth";
+const REPO_NAME = "/EPSE-Thrive/";
 
 export default defineConfig(({ command }) => ({
   base: command === "build" ? REPO_NAME : "/",
